701613471da8efb0f77784e8536b5d8c3980bd59
Miguel Mercedes <lmyguel0324@gmail.com>
2023-10-28 01:38:01 -0400

    project finished

---
+++ utils/helpers.ts
@@ -0,0 +1,8 @@
+export const formatearFecha = (fecha: string): string => {
+  const fechaNueva = new Date(fecha);
+  return fechaNueva.toLocaleDateString('es-ES', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+  });
+};
